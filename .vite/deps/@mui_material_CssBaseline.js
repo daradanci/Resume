a8fcd3e9aa6c9a802b5cd7def3c196c8ca6b64a4
daradanci <daradanci@gmail.com>
2025-03-10 20:59:18 +0300

    added base to url

---
+++ .vite/deps/@mui_material_CssBaseline.js
@@ -0,0 +1,9 @@
+import {
+  CssBaseline_default
+} from "./chunk-AZJ3G37C.js";
+import "./chunk-YQLRJNIK.js";
+import "./chunk-XZR65GWF.js";
+import "./chunk-UCZTVSZO.js";
+export {
+  CssBaseline_default as default
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProviderWrapper } from "./context/ThemeContext";
 import ThemeToggle from "./components/ThemeToggle";
 import AnimatedBackground from "./components/backgrounds/AnimatedBackground";
@@ -6,9 +7,13 @@ import Home from "./pages/Home";
 export default function App() {
   return (
     <ThemeProviderWrapper>
-      <AnimatedBackground />
-      <ThemeToggle />
-      <Home />
+      <Router basename="/repository-name">
+        <AnimatedBackground />
+        <ThemeToggle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </ThemeProviderWrapper>
   );
 }
